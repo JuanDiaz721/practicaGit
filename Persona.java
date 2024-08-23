3444bfbd1b66e3134fd86aa71bd2ba8389293ac9
Juan <juanito@gmail.com>
2024-08-23 08:34:41 -0500

    v2

--- Persona.java
+++ Persona.java
@@ -3,4 +3,18 @@ public class Persona(){
     private int id;
     private int age;
     private double salary;
+
+    public Persona(){
+    }
+    public Persona(string cedula, int id, int age, double salary){
+        this.name = name;
+        this.id=id;
+        this.age=age;
+        this.salary=salary;
+    }
+
+
+
+
 }
+
